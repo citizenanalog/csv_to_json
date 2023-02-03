54273507643ac94f2560096b47b9c5908f9ac4c7
Sharpe <Matt.Sharpe@Emerson.com>
2023-02-03 12:50:23 -0700

    added lookup fn.

--- src/main.rs
+++ src/main.rs
@@ -8,26 +8,48 @@ use std::env;
 #[derive(Deserialize, Debug, PartialEq, Eq, serde::Serialize)]
 struct Register {
     reg_type: String,
-    addr: i32,
+    addr: u32,
     descrip: String,
 }
 
+enum MapElement {
+    regtype(String),
+    regaddr(u16),
+    regdescrip(String)
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let path = env::current_dir()?;
+    let path: std::path::PathBuf = env::current_dir()?;
     println!("The current directory is {}", path.display());
-    let mut file = File::open("map5.csv").unwrap();
-    let mut csv_string = String::new();
+    let mut file: File = File::open("map5.csv").unwrap();
+    let mut csv_string: String = String::new();
     file.read_to_string(&mut csv_string).unwrap();
-    let csv_bytes = csv_string.as_bytes();
-    let mut rdr = Reader::from_reader(csv_bytes);
+    let csv_bytes: &[u8] = csv_string.as_bytes();
+    let mut rdr: Reader<&[u8]> = Reader::from_reader(csv_bytes);
     //#TODO - handle empty fields
     let records: Vec<Register> = rdr.deserialize().collect::<Result<_, _>>()?;
-    let _json = to_string(&records)?;
+    let _json: String = to_string(&records)?;
     //print the first 10
-    for i in 1..1000 {
+    //print_all(&records);
+    find_by_reg(32, &records);
+    /*for i in 1..records.len() {
         println!("records: {:#?}", records[i]);
-    }
+    }*/
     //println!("{:#?}", json);
     Ok(())
 }
 
+fn print_all(rec: &Vec<Register>) {
+    for i in 1..rec.len() {
+        println!("records: {:#?}", rec[i]);
+    }
+}
+
+fn find_by_reg(n: u32, rec: &Vec<Register>) {
+    for i in 1..rec.len() {
+        if rec[i].addr == n {
+            println!("found record: {:#?}", rec[i]);
+        }
+        
+    }
+}
